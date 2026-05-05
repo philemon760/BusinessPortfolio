92d3222cf1559b5699ef557c700d51cf6abf7d9d
philemon760 <Philemon.ntow@gmail.com>
2026-05-06 02:31:20 +0500

    'update-content'

--- components/experience.tsx
+++ components/experience.tsx
@@ -13,7 +13,7 @@ import { useTheme } from "@/context/theme-context";
 import { motion } from "framer-motion";
 
 export default function Experience() {
-  const { ref } = useSectionInView("Experience");
+  const { ref } = useSectionInView("Journey");
   const { theme } = useTheme();
 
   return (
--- components/skills.tsx
+++ components/skills.tsx
@@ -38,7 +38,7 @@ const fadeInAnimationVariants = {
 };
 
 export default function Skills() {
-  const { ref } = useSectionInView("Skills");
+  const { ref } = useSectionInView("Expertise");
 
   return (
     <section
